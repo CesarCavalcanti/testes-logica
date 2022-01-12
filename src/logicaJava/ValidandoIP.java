bdceedb79a579b09e93d3eb2ca5213157361d87d
CesarCavalcanti <senadevtrd@gmail.com>
2022-01-12 20:48:38 -0300

    Validando um ip com regex

---
+++ src/logicaJava/ValidandoIP.java
@@ -0,0 +1,10 @@
+package logicaJava;
+
+public class ValidandoIP {
+
+    public static void main(String[] args) {
+        String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+        String pattern = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
+    }
+}
+
