567357d15706ae5d190e8bcb162a2778cee91c24
CesarCavalcanti <senadevtrd@gmail.com>
2022-01-13 20:00:06 -0300

    Validando um usuario login com regex

---
+++ src/logicaJava/ValidandoLogin.java
@@ -0,0 +1,13 @@
+package logicaJava;
+
+public class ValidandoLogin {
+
+    public static void main(String[] args) {
+
+        // Valide login, não pode ter menos de 8 e nem mais de 30 caracteres, não pode ter sinais especiais e o primeiro caractere deve ser uma letra.
+
+
+        String validacao = ("([A-Za-z]{1})(\\w{7,29})");
+
+    }
+}
