342bce5b38da8bff17e94eb24b80c9f6aee0c949
CesarCavalcanti <senadevtrd@gmail.com>
2022-01-30 12:57:56 -0300

    Big integer

---
+++ src/logicaJava/SomaNumerosGrandes.java
@@ -0,0 +1,19 @@
+package logicaJava;
+
+import java.math.BigInteger;
+
+public class SomaNumerosGrandes {
+
+    public static void main(String[] args) {
+        BigInteger n1 = new BigInteger("1234");
+        BigInteger n2 = new BigInteger("20");
+
+        System.out.println(n1.multiply(n2));
+        System.out.println(n1.add(n2));
+
+    }
+
+
+
+
+}
